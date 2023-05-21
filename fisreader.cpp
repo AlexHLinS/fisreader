7db7898f11745e1ddf4bf79d2a481e979ceedb4e
Alex Shkil <alexhlins@gmail.com>
2023-05-21 23:52:16 +0300

    started body reader dev

---
+++ fisreader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Алексей Шкиль on 12.04.2022.
+//
+
+#include "fisreader.h"
---
+++ fisreader.h
@@ -0,0 +1,8 @@
+//
+// Created by Алексей Шкиль on 12.04.2022.
+//
+
+#ifndef FISREADER_FISREADER_H
+#define FISREADER_FISREADER_H
+
+#endif //FISREADER_FISREADER_H
